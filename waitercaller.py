a0ec8ff6495ed0600f36c8af6c9b82a12e37a144
Steven <steven_ni@yahoo.com>
2021-12-28 17:36:00 +0000

    inital setup

---
+++ waitercaller.py
@@ -0,0 +1,13 @@
+# flask-by-example
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Under construction"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
